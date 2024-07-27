ef6aa0d3ba8295e23d6c01108dc94caf22f9123b
Thiago Menezes <thiagomvas@gmail.com>
2024-07-27 18:04:39 -0300

    Add desktop project setup

---
+++ CircuitSim.Desktop/Program.cs
@@ -0,0 +1,14 @@
+﻿using Raylib_cs;
+using static Raylib_cs.Raylib;
+
+InitWindow(800, 450, "CircuitSim");
+SetTargetFPS(60);
+
+while (!WindowShouldClose())
+{
+    BeginDrawing();
+    ClearBackground(Color.DarkGray);
+    DrawText("Congrats! You created your first window!", 190, 200, 20, Color.RayWhite);
+    EndDrawing();
+}
+CloseWindow();
