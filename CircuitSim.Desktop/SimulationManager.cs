7e1d12efb046b707b4d3e32fc5e792e712179e61
Thiago Menezes <thiagomvas@gmail.com>
2024-07-27 18:16:10 -0300

    Add basic SimulationManager

---
+++ CircuitSim.Desktop/SimulationManager.cs
@@ -0,0 +1,28 @@
+﻿using CircuitSim.Core.Common;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+
+namespace CircuitSim.Desktop
+{
+    internal class SimulationManager
+    {
+        private bool isDrawing = false;
+        public List<Wire> Wires { get; set; } = new();
+
+        public void Update()
+        {
+            foreach (var wire in Wires)
+            {
+                wire.Flow();
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (var wire in Wires)
+            {
+                DrawLineEx(wire.Start, wire.End, 2, Color.RayWhite);
+            }
+        }
+    }
+}
