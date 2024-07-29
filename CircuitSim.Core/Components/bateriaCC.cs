c39870f6f1a290ce5baef61e2d1333095b5e8f16
Rafael Gomes <rafaelgomes8994@gmail.com>
2024-07-28 21:40:38 -0300

    add uma nova classe de bateria CC onde a classe assume que a tensão fornecida é unidirecional e constante, o que é típico de uma bateria monopolo de corrente contínua. Não há configurações para tensão alternada ou múltiplos terminais, então o conceito de monopolo e corrente contínua está implícito no design da classe.

---
+++ CircuitSim.Core/Components/bateriaCC.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CircuitSim.Core.Common;
+
+namespace CircuitSim.Core.Components
+{
+    public class bateriaCC : Wire
+    {
+
+        public double VoltageOutput { get; set; }
+        public double Capacity { get; set; }
+
+        public bool IsActive => Capacity > 0;
+
+        public override void Flow()
+        {
+            if (IsActive)
+            {
+                Capacity -= VoltageOutput * 0.1;
+                if (Capacity < 0) Capacity = 0;
+
+                Voltage = VoltageOutput;
+
+                base.Flow();
+            }
+        }
+    }
+}
+
