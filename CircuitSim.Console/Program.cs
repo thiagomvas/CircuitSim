e17f232c8bdba9820b64d5246839d51d68291566
Thiago Menezes <thiagomvas@gmail.com>
2024-07-31 14:05:25 -0300

    Add Circuit class for holding serialization and deserialization

--- CircuitSim.Console/Program.cs
+++ CircuitSim.Console/Program.cs
@@ -1,9 +1,33 @@
 ﻿using CircuitSim.Core.Common;
-using System.Reflection;
+using CircuitSim.Core.Components;
+using System.Numerics;
 
-var types = Assembly.GetAssembly(typeof(Wire))
-    .GetTypes()
-    .Where(t => t.IsAssignableTo(typeof(Wire)));
+Circuit circuit = new();
+circuit.Wires.Add(new VoltageSource() { SupplyVoltage = 10, Start = Vector2.Zero, End = new(1, 0) });
+circuit.Wires.Add(new Resistor() { Resistance = 1000, Start = new(1, 0), End = new(1, 1) });
+circuit.Wires.Add(new Wire() { Start = new(1, 1), End = new(2, 1) });
 
-foreach (var type in types)
-    Console.WriteLine(type.Name);
+var json = circuit.SerializeToJson();
+
+Console.WriteLine(json);
+
+var deserialized = Circuit.DeserializeFromJson(json);
+
+foreach (var wire in deserialized.Wires)
+{
+    var type = wire.GetType();
+    if(type == typeof(VoltageSource))
+    {
+        var vs = (VoltageSource)wire;
+        Console.WriteLine($"Voltage Source: {vs.SupplyVoltage}V");
+    }
+    else if(type == typeof(Resistor))
+    {
+        var res = (Resistor) wire;
+        Console.WriteLine($"Resistor: {res.Resistance} Ohms");
+    }
+    else if(type == typeof(Wire))
+    {
+        Console.WriteLine("Wire");
+    }
+}
---
+++ CircuitSim.Core/Common/Circuit.cs
@@ -0,0 +1,71 @@
+﻿using CircuitSim.Core.DTOs;
+using Newtonsoft.Json.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CircuitSim.Core.Common
+{
+    public class Circuit
+    {
+        public List<Wire> Wires { get; set; } = new List<Wire>();
+
+        public string SerializeToJson()
+        {
+            var entity = new JObject();
+            var dtos = Wires.Select(w => w.ToDTO());
+            var jarray = JArray.FromObject(dtos);
+            entity["Wires"] = JArray.FromObject(dtos);
+            return entity.ToString(Newtonsoft.Json.Formatting.Indented);
+        }
+
+        public static Circuit DeserializeFromJson(string json)
+        {
+            var entity = JObject.Parse(json);
+            var circuit = new Circuit();
+
+            // Converting the JSON to a list of WireDTOs
+            var dtos = entity["Wires"].ToObject<List<WireDTO>>();
+            foreach(var wire in entity["Wires"])
+            {
+                // Parsing the wire type
+                var type = ByName(wire["Type"]?.Value<string>()?.Split(',').First() ?? string.Empty);
+                var props = type.GetProperties();
+
+                // Creating an instance of that type
+                var instance = Activator.CreateInstance(type);
+
+                // Defining the properties of the instance using Reflection
+                foreach (var prop in wire["Data"] as JObject)
+                {
+                    if(props.Any(p => p.Name == prop.Key))
+                    {
+                        var propInfo = props.First(p => p.Name == prop.Key);
+                        if(propInfo.CanWrite)
+                            propInfo.SetValue(instance, prop.Value.ToObject(propInfo.PropertyType));
+                    }
+                }
+                
+                // After all that headache, add to the circuit.
+                circuit.Wires.Add((Wire) instance);
+            }
+            return circuit;
+        }
+
+        private static Type? ByName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return null;
+
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies().Reverse())
+            {
+                var tt = assembly.GetType(name);
+                if (tt != null)
+                {
+                    return tt;
+                }
+            }
+
+            return null;
+        }
+    }
+}
