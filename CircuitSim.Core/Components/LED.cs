94a78630d3faa8969d78215f06fa57b0ce893469
Thiago Menezes <thiagomvas@gmail.com>
2024-08-09 14:02:00 -0300

    LED can now change colors

--- CircuitSim.Core/Components/LED.cs
+++ CircuitSim.Core/Components/LED.cs
@@ -1,4 +1,5 @@
-﻿using CircuitSim.Core.Common;
+﻿using CircuitSim.Core.Annotations;
+using CircuitSim.Core.Common;
 using System.Drawing;
 
 namespace CircuitSim.Core;
@@ -13,12 +14,23 @@ public class LED : Wire
     /// </summary>
     public double VoltageConsumption { get; set; } = 5;
 
+    private byte r = 0xff;
+    private byte g = 0xff;
+    private byte b = 0xff;
+
     public Color LitColor { get; set; } = Color.White;
 
     /// <summary>
     /// Gets a value indicating whether the LED is on.
     /// </summary>
     public bool IsOn => Voltage > VoltageConsumption;
+
+    [PropertyEditable]
+    public byte R { get => r; set { r = value; LitColor = Color.FromArgb(r, g, b); } }
+    [PropertyEditable]
+    public byte G { get => g; set { g = value; LitColor = Color.FromArgb(r, g, b); } }
+    [PropertyEditable]
+    public byte B { get => b; set { b = value; LitColor = Color.FromArgb(r, g, b); } }
 
     /// <inheritdoc/>
     public override void Flow()
